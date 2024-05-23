cdff937f6a4b3e96b1f5d100fe192b2f402779ce
hasundue <hasundue@gmail.com>
2024-05-23 15:13:31 +0900

    feat: add `lock` input

--- src/params_test.ts
+++ src/params_test.ts
@@ -10,6 +10,7 @@ Deno.test("fromInputs - default source (deno.json)", async () => {
     }),
     {
       prefix: "chore: ",
+      lock: "deno.lock",
       root: "test/fixtures",
       source: ["deno.json"],
     },
@@ -23,6 +24,7 @@ Deno.test("fromInputs - default source (modules)", async () => {
       root: "src",
     }),
     {
+      lock: undefined,
       prefix: "chore: ",
       root: "src",
       source: ["./**/*.ts"],
@@ -38,6 +40,7 @@ Deno.test("fromInputs - explicit sources", async () => {
       source: ["mod.ts", "deps.ts"],
     }),
     {
+      lock: "deno.lock",
       prefix: "chore: ",
       root: "test/fixtures",
       source: ["mod.ts", "deps.ts"],
@@ -49,6 +52,7 @@ Deno.test("fromInputs - default inputs", async () => {
   assertObjectMatch(
     await fromInputs(defaults),
     {
+      lock: "deno.lock",
       prefix: "chore: ",
       resolve: false,
       root: ".",
